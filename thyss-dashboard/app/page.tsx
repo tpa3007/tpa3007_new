46ba15d1df5875baae2a39ce9820bf8e7a43d5de
tpa3007 <tpa300701@gmail.com>
2025-12-26 17:34:47 +0300

    Delete thyss-dashboard directory

--- thyss-dashboard/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center font-mono">
-      <h1 className="text-4xl font-black uppercase italic tracking-tighter shadow-sm">
-        THYSS_CORE_READY
-      </h1>
-    </div>
-  );
-}
